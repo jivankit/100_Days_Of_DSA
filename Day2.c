d1a3b39e1ac7953ce89e250d96a7fad18678f151
Jivankit <jivankit@gmail.com>
2026-03-03 12:00:00 +0530

    Day 2

---
+++ Day2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, pos;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &pos);
+
+    for(int i = pos-1; i < n-1; i++)
+        arr[i] = arr[i+1];
+
+    n--;
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
